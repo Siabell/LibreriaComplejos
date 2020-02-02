b35619f7779b17fbf1d9ebf42c5ac28abc9ba245
Siabell <valesiabatto@gmail.com>
2020-02-02 18:07:28 -0500

    complex numbers test

--- src/main/ComplexNumber.py
+++ src/main/ComplexNumber.py
@@ -22,20 +22,20 @@ class ComplexNumber:
 
     def division(nComplex1,nComplex2):
         real=(nComplex1.partReal*nComplex2.partReal)+(nComplex1.partImag*nComplex2.partImag)
-        debajo=(pow(nComplex2.partReal)+pow(nComplex2.partImag))
+        debajo=(pow(nComplex2.partReal,2)+pow(nComplex2.partImag,2))
         imag=(nComplex1.partReal*nComplex2.partImag)-(nComplex1.partImag*nComplex2.partReal)
         partereal= real/debajo
         parteImag= imag/debajo
         return ComplexNumber(partereal,parteImag)
 
     def modulus(nComplex):
-        return math.sqrt(pow(nComplex.partReal)+pow(nComplex.partImag))
+        return math.sqrt(pow(nComplex.partReal,2)+pow(nComplex.partImag,2))
 
     def conjugate(nComplex):
         return ComplexNumber(nComplex.partReal,nComplex.partImag*(-1))
 
     def cartesianToPolar(nComplex):
-        p = math.sqrt(pow(nComplex.partReal)+pow(nComplex.partImag))
+        p = math.sqrt(pow(nComplex.partReal,2)+pow(nComplex.partImag,2))
         o = math.atan(nComplex.partReal/nComplex.partImag)
         return (p,o)
 
--- src/main/ComplexNumberTest.py
+++ src/main/ComplexNumberTest.py
@@ -10,5 +10,47 @@ class ComplexNumberTest(unittest.TestCase):
         self.assertEqual(cResultTest.partReal,4)
         self.assertEqual(cResultTest.partImag,3)
 
+    def testShouldSubtractTwoComplexNumbers(self):
+        c1 = complex.ComplexNumber(7, 9)
+        c2 = complex.ComplexNumber(3,4)
+        cResultTest = c1.subtract(c2)
+        self.assertEqual(cResultTest.partReal,4)
+        self.assertEqual(cResultTest.partImag,5)
+
+    def testShouldMultiplyTwoComplexNumbers(self):
+        c1 = complex.ComplexNumber(3, -1)
+        c2 = complex.ComplexNumber(1,4)
+        cResultTest = c1.multiplication(c2)
+        self.assertEqual(cResultTest.partReal,7)
+        self.assertEqual(cResultTest.partImag,11)
+
+    def testShouldDivideTwoComplexNumbers(self):
+        c1 = complex.ComplexNumber(4, 5)
+        c2 = complex.ComplexNumber(2,6)
+        cResultTest = c1.division(c2)
+        self.assertAlmostEqual(cResultTest.partReal,0.95)
+        self.assertAlmostEqual(cResultTest.partImag,0.35)
+
+    def testShouldGetConjugateOfComplexNumber(self):
+        c1 = complex.ComplexNumber(4, -5)
+        cResultTest = c1.conjugate()
+        self.assertEqual(cResultTest.partImag,5)
+
+    def testShouldGetModulusOfComplexNumber(self):
+        c1 = complex.ComplexNumber(6, 8)
+        cResultTest = c1.modulus()
+        self.assertEqual(cResultTest,10)
+
+    def testShouldGetPhaseOfComplexNumber(self):
+        c1 = complex.ComplexNumber(1, 1)
+        cResultTest = c1.phase()
+        self.assertAlmostEqual(cResultTest,0.78539816339)
+
+    def testShouldGetPolarRepresentationOfComplexNumber(self):
+        c1 = complex.ComplexNumber(1, 1)
+        cResultTest = c1.cartesianToPolar()
+        self.assertAlmostEqual(cResultTest[0],1.41421356237)
+        self.assertAlmostEqual(cResultTest[1],0.7853981633)
+
 if __name__ == '__main__':
     unittest.main()
