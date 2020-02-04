1bd3229930b8d105415dfb589359676bdb233b2b
Siabell <valesiabatto@gmail.com>
2020-02-04 12:21:28 -0500

    first commit

--- src/main/Matrix.py
+++ src/main/Matrix.py
@@ -8,38 +8,122 @@ class Matrix:
         self.m = len(pmatrix) #rows
         self.n = len(pmatrix) #column
 
-    def add (mat1, mat2):
-        if (mat1.m != mat2.m and mat1.n != mat2.n):
+    def add (self, mat2):
+        """ Suma de dos matrices """
+        if (self.m != mat2.m and self.n != mat2.n):
             raise Exception('The dimensions of the matrices are not the same ')
         else:
-            matrixResult = [[complex.ComplexNumber(0,0) for x in range(mat1.m)] for y in range(mat1.n)] 
-            for i in range (mat1.m):
-                for j in range (mat1.n):
-                    matrixResult[i][j] = mat1.mtx[i][j].add(mat2.mtx[i][j])
+            matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+            for i in range (self.m):
+                for j in range (self.n):
+                    matrixResult[i][j] = self.mtx[i][j].add(mat2.mtx[i][j])
             matResult = Matrix(matrixResult)
             return matResult
 
-    def inverse (mat)
-        matrixResult = [[complex.ComplexNumber(0,0) for x in range(mat.m)] for y in range(mat.n)] 
-            for i in range (mat.m):
-                for j in range (mat.n):
-                    matrixResult[i][j] = mat.mtx[i][j].inverse()
+    def subtract (self, mat2):
+        """ Resta de dos matrices """
+        if (self.m != mat2.m and self.n != mat2.n):
+            raise Exception('The dimensions of the matrices are not the same ')
+        else:
+            matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+            for i in range (self.m):
+                for j in range (self.n):
+                    matrixResult[i][j] = self.mtx[i][j].subtract(mat2.mtx[i][j])
+            matResult = Matrix(matrixResult)
+            return matResult
+
+    def inverse (self):
+        """ Inversa de una matriz"""
+        matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+        for i in range (self.m):
+            for j in range (self.n):
+                matrixResult[i][j] = self.mtx[i][j].inverse()
         matResult = Matrix(matrixResult)
         return matResult
     
-    def 
+    def scalarMultiplication(self,c):
+        """ Producto escalar de una matrix y un numero complejo c """
+        matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+        for i in range (self.m):
+            for j in range (self.n):
+                matrixResult[i][j]=self.mtx[i][j].multiplication(c)
+        matResult = Matrix(matrixResult)
+        return matResult
 
-    def equals(mat1,mat2):
-            result = True
-            if (mat1.m!= mat2.m or mat1.n != mat2.n):
-                raise Exception('The dimensions of the matrices are not the same ')
-            for i in range(mat1.m):
-                for j in range(mat1.n):
-                    if(mat1.mtx[i][j].partReal != mat2.mtx[i][j].partReal or mat1.mtx[i][j].partImag != mat2.mtx[i][j].partImag):
-                        result = False
-            return result
+    def multiplication(self, mat2):
+        """ multiplicacion de dos matrices """
+        if (self.m != mat2.n ):
+            raise Exception('The dimensions of rows and columns do not match')
+        else:
+            matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+            for i in range (self.m):
+                for j in range (mat2.n):
+                    sum = complex.ComplexNumber(0,0)
+                    for z in range(self.n):
+                        sum = sum.add(self.mtx[i][z].multiplication(mat2.mtx[z][j]))
+                    matrixResult[i][j] = sum
+            matResult = Matrix(matrixResult)
+            return matResult
 
-    def showMatrix(mat):
-        for i in range(mat.m):
-                for j in range(mat.n):
-                    print(mat.mtx[i][j].partReal,mat.mtx[i][j].partImag)
+    def transpose (self) :
+        """ Transpuesta de una matriz"""
+        matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.n)] for y in range(self.m)] 
+        for i in range (self.m):
+            for j in range (self.n):
+                matrixResult[j][i] = self.mtx[i][j]
+        matResult = Matrix(matrixResult)
+        return matResult
+    
+    def conjugate (self):
+        """ Conjuagada de una matriz"""
+        matrixResult = [[complex.ComplexNumber(0,0) for x in range(self.m)] for y in range(self.n)] 
+        for i in range (self.m):
+            for j in range (self.n):
+                matrixResult[i][j] = self.mtx[i][j].conjugate()
+        matResult = Matrix(matrixResult)
+        return matResult
+
+    def adjoint (self):
+        """ Adjunta de una matriz que es la tranpuesta de la conjugada"""
+        return self.conjugate().transpose()
+
+    def trace (self):   
+        """ Traza de una matriz: la suma de los elementos de la diagonal"""
+        if (self.m != self.n ):
+            raise Exception('The dimensions of rows and columns do not match')
+        else:
+            sum = complex.ComplexNumber(0,0)
+            for i in range (self.m):
+                sum = sum.add(self.mtx[i][i])
+            print(sum.partReal)
+            return sum.partReal
+
+    def innerProduct(self,mat2):
+        """ Producto interno de una matriz: la traza del resultado de 
+            la multiplicacion de las mattriz adjunta y la segunda matriz"""
+        adjunta = self.adjoint()
+        matResult = adjunta.multiplication(mat2)
+        return matResult.trace()
+
+    def norm(self):
+        """ norma de una matriz: raiz del producto interno de la misma matriz """
+        result = math.sqrt(self.innerProduct(self))
+        return round(result,3)
+        
+
+    def equals(self,mat2):
+        """ Compara si los elementos de las matrices son iguales """
+        result = True
+        if (self.m!= mat2.m or self.n != mat2.n):
+            raise Exception('The dimensions of the matrices are not the same ')
+        for i in range(self.m):
+            for j in range(self.n):
+                if(self.mtx[i][j].partReal != mat2.mtx[i][j].partReal or self.mtx[i][j].partImag != mat2.mtx[i][j].partImag):
+                    result = False
+        return result
+
+    def showMatrix(self):
+        for i in range(self.m):
+            for j in range(self.n):
+                print(self.mtx[i][j].partReal,self.mtx[i][j].partImag)
+            print("-----")
--- src/main/MatrixTest.py
+++ src/main/MatrixTest.py
@@ -11,6 +11,48 @@ class ComplexNumberTest(unittest.TestCase):
         mSol =  matrix.Matrix([[complex.ComplexNumber(0, -1), complex.ComplexNumber(6, 4)], [complex.ComplexNumber(4, 8), complex.ComplexNumber(-1, 3)]])
         self.assertTrue(mTest.equals(mSol))
 
+    def testShouldSubtractTwoMatrices(self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(-1, -1), complex.ComplexNumber(3, 0)], [complex.ComplexNumber(4, 6), complex.ComplexNumber(2, 2)]])
+        m2 = matrix.Matrix([[complex.ComplexNumber(1, 0), complex.ComplexNumber(3, 4)], [complex.ComplexNumber(0, 2), complex.ComplexNumber(-3, 1)]])
+        mTest = m1.subtract(m2)
+        mSol =  matrix.Matrix([[complex.ComplexNumber(-2, -1), complex.ComplexNumber(0, -4)], [complex.ComplexNumber(4, 4), complex.ComplexNumber(5, 1)]])
+        self.assertTrue(mTest.equals(mSol))
+
+    #def testInverseMatrices(self):
+        
+    def testMatricesMultiplication(self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(3, 2), complex.ComplexNumber(0, 0),complex.ComplexNumber(5, -6)], [complex.ComplexNumber(1, 0), complex.ComplexNumber(4, 2),complex.ComplexNumber(0, 1)],[complex.ComplexNumber(4, -1), complex.ComplexNumber(0, 0),complex.ComplexNumber(4, 0)]])
+        m2 = matrix.Matrix([[complex.ComplexNumber(5, 0), complex.ComplexNumber(2, -1),complex.ComplexNumber(6, -4)], [complex.ComplexNumber(0, 0), complex.ComplexNumber(4, 5),complex.ComplexNumber(2, 0)],[complex.ComplexNumber(7, -4), complex.ComplexNumber(2, 7),complex.ComplexNumber(0, 0)]])
+        mTest = m1.multiplication(m2)
+        mSol =  matrix.Matrix([[complex.ComplexNumber(26, -52), complex.ComplexNumber(60, 24),complex.ComplexNumber(26, 0)], [complex.ComplexNumber(9, 7), complex.ComplexNumber(1, 29),complex.ComplexNumber(14, 0)],[complex.ComplexNumber(48, -21), complex.ComplexNumber(15, 22),complex.ComplexNumber(20, -22)]])
+        self.assertTrue(mTest.equals(mSol))
+
+    def testMatrixTranspose(self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(6, -3), complex.ComplexNumber(2, 12),complex.ComplexNumber(0, -19)], [complex.ComplexNumber(0, 0), complex.ComplexNumber(5, 2.1),complex.ComplexNumber(17, 0)],[complex.ComplexNumber(1, 0), complex.ComplexNumber(2, 5),complex.ComplexNumber(3, -4.5)]])
+        mTest = m1.transpose()
+        mSol =  matrix.Matrix([[complex.ComplexNumber(6, -3), complex.ComplexNumber(0, 0),complex.ComplexNumber(1, 0)], [complex.ComplexNumber(2, 12), complex.ComplexNumber(5, 2.1),complex.ComplexNumber(2, 5)],[complex.ComplexNumber(0, -19), complex.ComplexNumber(17, 0),complex.ComplexNumber(3, -4.5)]])
+        self.assertTrue(mTest.equals(mSol))
+
+    def testMatrixConjugate(self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(6, -3), complex.ComplexNumber(2, 12),complex.ComplexNumber(0, -19)], [complex.ComplexNumber(0, 0), complex.ComplexNumber(5, 2.1),complex.ComplexNumber(17, 0)],[complex.ComplexNumber(1, 0), complex.ComplexNumber(2, 5),complex.ComplexNumber(3, -4.5)]])
+        mTest = m1.conjugate()
+        mSol =  matrix.Matrix([[complex.ComplexNumber(6, 3), complex.ComplexNumber(2, -12),complex.ComplexNumber(0, 19)], [complex.ComplexNumber(0, 0), complex.ComplexNumber(5, -2.1),complex.ComplexNumber(17, 0)],[complex.ComplexNumber(1, 0), complex.ComplexNumber(2, -5),complex.ComplexNumber(3, 4.5)]])
+        self.assertTrue(mTest.equals(mSol))
+
+    def testMatrixAdjoint(self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(6, -3), complex.ComplexNumber(2, 12),complex.ComplexNumber(0, -19)], [complex.ComplexNumber(0, 0), complex.ComplexNumber(5, 2.1),complex.ComplexNumber(17, 0)],[complex.ComplexNumber(1, 0), complex.ComplexNumber(2, 5),complex.ComplexNumber(3, -4.5)]])
+        mTest = m1.adjoint()
+        mSol =  matrix.Matrix([[complex.ComplexNumber(6, 3), complex.ComplexNumber(0, 0),complex.ComplexNumber(1, 0)], [complex.ComplexNumber(2, -12), complex.ComplexNumber(5, -2.1),complex.ComplexNumber(2, -5)],[complex.ComplexNumber(0, 19), complex.ComplexNumber(17, 0),complex.ComplexNumber(3, 4.5)]])
+        self.assertTrue(mTest.equals(mSol))
+    
+    #def testInnerProductMatrices(self):
+    '''
+    def testNorm (self):
+        m1 = matrix.Matrix([[complex.ComplexNumber(4, 3), complex.ComplexNumber(6,-4),complex.ComplexNumber(12, -7), complex.ComplexNumber(0, 13)]])
+        mTest = m1.norm()
+        mSol =  6.855
+        self.assertEquals(mTest, mSol) '''
+
 
 if __name__ == '__main__':
     unittest.main()
